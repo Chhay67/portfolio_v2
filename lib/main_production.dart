9fa7bdec4a57f4007baf2a50ee17741db3553ee4
chhay <songkimchhay67@gmail.com>
2025-08-06 21:19:06 +0700

    set up cloud_firestore

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -7,6 +7,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:portfolio_v2/root_app.dart';
 
 import 'core/config/app_config.dart';
+import 'core/service/cloud_firestore_service.dart';
 
 void main() async {
   runZonedGuarded(
@@ -15,6 +16,8 @@ void main() async {
       setUrlStrategy(PathUrlStrategy());
       AppConfig.appFlavor = Flavor.production;
       await Firebase.initializeApp(options: AppConfig.firebaseOptions);
+
+      await CloudFirestoreService.trackAppOpenOnce();
       runApp( ProviderScope(child: RootApp(title: AppConfig.appName)));
     },
     (error, stackTrace) {
